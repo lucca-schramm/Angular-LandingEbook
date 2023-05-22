5509d63df54f2c622b708aa1ce24fba0ba478ce9
Lucca Schramm <luccascho@gmail.com>
2023-05-22 11:36:16 -0300

    Tornando o menu-bar reutilizável e permitindo a inserção de dados pelo HTML raiz

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent {
+  @Input()
+  primeiroLink:string="";
+  @Input()
+  segundoLink:string="";
 
 }

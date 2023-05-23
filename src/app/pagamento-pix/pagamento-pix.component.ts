09a3a2f447e0b773bb63f0ed4af65b2dd37bed32
Lucca Schramm <luccascho@gmail.com>
2023-05-23 11:41:01 -0300

    Adicionando componente estilizado de pagamento via mercado pago

---
+++ src/app/pagamento-pix/pagamento-pix.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-pagamento-pix',
+  templateUrl: './pagamento-pix.component.html',
+  styleUrls: ['./pagamento-pix.component.css']
+})
+export class PagamentoPixComponent {
+
+}
